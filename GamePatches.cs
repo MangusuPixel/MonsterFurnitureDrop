89f89acb07877bdd4ba30e69c64813ee97013944
MangusuPixel <mangusupixel@gmail.com>
2024-06-01 19:02:03 -0400

    Initial commit

---
+++ GamePatches.cs
@@ -0,0 +1,45 @@
+﻿using HarmonyLib;
+using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Monsters;
+
+namespace MonsterFurnitureDrop
+{
+    internal static class GamePatches
+    {
+        internal static void Apply(Harmony harmony)
+        {
+            try
+            {
+                harmony.Patch(
+                    original: AccessTools.Method(typeof(GameLocation), nameof(GameLocation.monsterDrop)),
+                    prefix: new HarmonyMethod(typeof(GamePatches), nameof(MonsterDrop_Prefix))
+                );
+            }
+            catch (Exception e)
+            {
+                ModEntry.SMonitor.Log($"Failed patching game:\n{e}", LogLevel.Error);
+            }
+        }
+
+        internal static bool MonsterDrop_Prefix(GameLocation __instance, Monster monster, int x, int y, Farmer who)
+        {
+            var monsterDropRate = ModEntry.Config.MonsterDropRates.ContainsKey(monster.Name) ? ModEntry.Config.MonsterDropRates[monster.Name] : 0;
+            var dropChance = ModEntry.Config.GlobalDropChance * monsterDropRate;
+
+            if (ModEntry.ItemDropPool.Count > 0 && dropChance > Game1.random.NextDouble())
+            {
+                var randKey = "(F)" + ModEntry.ItemDropPool.Next();
+                var itemInfo = ItemRegistry.GetDataOrErrorItem(randKey);
+
+                monster.objectsToDrop.Add(randKey);
+
+                if (ModEntry.Config.LogDropMessages)
+                    ModEntry.SMonitor.Log($"{monster.displayName} dropped '{itemInfo.DisplayName}'.", LogLevel.Info);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ ModConfig.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework.Input;
+using StardewModdingAPI;
+
+namespace MonsterFurnitureDrop
+{
+    public sealed class ModConfig
+    {
+        public float GlobalDropChance { get; set; }
+
+        public int DefaultItemDropRate { get; set; }
+
+        public float DefaultMonsterDropRate { get; set; }
+
+        public bool RemoveFurnitureCatalogue { get; set; }
+
+        public bool LogDropMessages { get; set; }
+
+        public Dictionary<string, int> ItemDropRates { get; set; } = new();
+
+        public Dictionary<string, float> MonsterDropRates { get; set; } = new();
+
+        public ModConfig()
+        {
+            ResetToDefaults();
+        }
+
+        public void ResetToDefaults()
+        {
+            GlobalDropChance = 0.01f;
+            RemoveFurnitureCatalogue = true;
+            LogDropMessages = false;
+
+            foreach (var entry in ItemDropRates)
+                ItemDropRates[entry.Key] = DefaultItemDropRate;
+
+            foreach (var entry in MonsterDropRates)
+                MonsterDropRates[entry.Key] = DefaultMonsterDropRate;
+        }
+    }
+}
